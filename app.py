901fa812b7f4a292569aca1c41797a63cda1a083
ulyssisss <ulyssisss@gmail.com>
2021-07-08 22:28:50 +0900

    apptesting

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.write("adsf")
+"""
+# 실행 테스트
+
+https://www.naver.com
+
+```python
+print("Hello World")
+```
+
+|         |   수학       |  평가       |
+|---------|:-----------:|:------------:|
+| 철수    |  90         | 참잘했어요. |
+| 영희    |  40         | 분발하세요. |
+
+"""
